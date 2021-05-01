a1ac37862e507599a5a137ef235199caa0721239
Brahim Benlarbi <brahim.benlarbi@wintech.it>
2021-05-01 14:10:45 +0200

    removed auto generated code

--- apps/shop-shoes/src/app/app.module.ts
+++ apps/shop-shoes/src/app/app.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'products',
+    pathMatch: 'full',
+  },
+];
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, RouterModule.forRoot(routes)],
   providers: [],
   bootstrap: [AppComponent],
 })
