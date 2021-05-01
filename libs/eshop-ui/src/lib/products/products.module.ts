67ba29167def1aeafd353e962cf2072ace5e8de7
Brahim Benlarbi <brahim.benlarbi@wintech.it>
2021-05-01 15:17:52 +0200

    adde produts service

--- libs/eshop-ui/src/lib/products/products.module.ts
+++ libs/eshop-ui/src/lib/products/products.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { ProductsComponent } from './containers/products/products.component';
 import { CartComponent } from './containers/cart/cart.component';
 import { ProductItemComponent } from './components/product-item/product-item.component';
 import { CartItemComponent } from './components/cart-item/cart-item.component';
 import { ProductComponent } from './containers/product/product.component';
+import { ProductsService } from './services/products.service';
 
 const routes: Routes = [
   {
@@ -31,6 +33,7 @@ const routes: Routes = [
     CartItemComponent,
     ProductComponent,
   ],
-  imports: [CommonModule, RouterModule.forChild(routes)],
+  imports: [CommonModule, RouterModule.forChild(routes), HttpClientModule],
+  providers: [ProductsService],
 })
 export class ProductsModule {}
