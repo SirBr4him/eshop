58bed7c1d4c9802f16037f960016c709f625cfbc
Brahim Benlarbi <brahim.benlarbi@wintech.it>
2021-05-02 23:20:28 +0200

    added header to app

--- apps/shop-shoes/src/app/app.module.ts
+++ apps/shop-shoes/src/app/app.module.ts
@@ -1,9 +1,11 @@
+import { core } from '@angular/compiler';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule, Routes } from '@angular/router';
 
 import { EshopUiModule } from '@eshop/eshop-ui';
+import { CoreModule } from '@eshop/eshop-ui/core';
 import { AppComponent } from './app.component';
 
 const routes: Routes = [
@@ -20,6 +22,7 @@ const routes: Routes = [
     BrowserAnimationsModule,
     RouterModule.forRoot(routes),
     EshopUiModule,
+    CoreModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
