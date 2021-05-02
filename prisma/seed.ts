d56953176fa8a86fa8dee3f73feb328e81c91fb0
Brahim Benlarbi <brahim.benlarbi@wintech.it>
2021-05-02 22:00:17 +0200

    added prisma and created a dev db

---
+++ prisma/seed.ts
@@ -0,0 +1,42 @@
+import { PrismaClient, Product } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+const items: Product[] = [
+  {
+    name: 'Air Jordan 4 “Super Nintendo”',
+    description: 'Great fit, super comfy',
+    image: 'https://i.imgur.com/cYeomek.png',
+    price: 255,
+    sku: '123',
+  },
+  {
+    name: 'Air jordan 4 Cement',
+    description: 'Awesome for cold days',
+    image: 'https://i.imgur.com/OGW9naM.jpeg',
+    price: 188,
+    sku: '456',
+  },
+  {
+    name: 'Air Jordan Off-White 5',
+    description: 'A little fancier',
+    image: 'https://i.imgur.com/BShhvX1.jpg',
+    price: 325,
+    sku: '789',
+  },
+];
+
+async function main() {
+  for (let item of items) {
+    await prisma.product.create({ data: item });
+  }
+}
+
+main()
+  .catch((err) => {
+    console.log(err);
+    process.exit(1);
+  })
+  .finally(() => {
+    prisma.$disconnect();
+  });
